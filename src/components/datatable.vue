892a3d92312f5b94c4ad904017ac0471ff511421
armmieyy <68525381+armmieyy@users.noreply.github.com>
2020-10-08 16:20:11 +0700

    init now

--- src/components/datatable.vue
+++ src/components/datatable.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <v-row no-gutters>
-      <v-col md="1"><v-card class="pa-2 blue darken-3" outlined> ID</v-card></v-col>
-      <v-col md="2"><v-card class="pa-2 blue darken-3" outlined> User name</v-card></v-col>
-      <v-col md="2"><v-card class="pa-2 blue darken-3" outlined> Password</v-card></v-col>
-      <v-col md="2"><v-card class="pa-2 blue darken-3" outlined> Phonenumber</v-card></v-col>
-      <v-col md="3"><v-card class="pa-2 blue darken-3" outlined> Email address</v-card></v-col>
-      <v-col md="1"><v-card class="pa-2 blue darken-3" outlined> Gender</v-card></v-col>
+      <v-col md="1"><v-card class="pa-2 blue lighten-2" outlined> ID</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2 blue lighten-2" outlined> User name</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2 blue lighten-2" outlined> Password</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2 blue lighten-2" outlined> Phonenumber</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2 blue lighten-2" outlined> Email address</v-card></v-col>
+      <v-col md="1"><v-card class="pa-2 blue lighten-2" outlined> Gender</v-card></v-col>
     </v-row>
     <v-row v-for="(items, index) in $store.getters.items" :key="index"  no-gutters>
       <v-col md="1"><v-card class="pa-2" outlined> {{ index + 1 }}</v-card></v-col>
